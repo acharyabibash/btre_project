32f268859fa34271cbf15227c33525480829b05a
Bibash Acharya <acharabibash@outlook.com>
2020-06-26 09:40:07 +0545

    33 Brad Incomplete on lot size

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank =True)
+    title = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2,decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IPAddressField()
+    lot_size = models.DecimalField(max_digits=5)
