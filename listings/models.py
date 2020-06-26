a7a604685fd367a0fd201c84629d8331525f3349
Bibash Acharya <acharabibash@outlook.com>
2020-06-26 09:56:52 +0545

    Fourth Commit : 33 Brad

--- listings/models.py
+++ listings/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
@@ -10,7 +12,18 @@ class Listing(models.Model):
     title = models.IntegerField()
     bedrooms = models.IntegerField()
     bedrooms = models.IntegerField()
-    bathrooms = models.DecimalField(max_digits=2,decimal_places=1)
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IPAddressField()
-    lot_size = models.DecimalField(max_digits=5)
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo1 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo2 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo3 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo4 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo5 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo6 = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
